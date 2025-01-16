53c37069b6fd8603b7a5bbbac9bdcdd6ccfabd12
jasir-pv <jasirahsanpv40@gmail.com>
2025-01-16 11:02:30 +0530

    tilte, description and bg gradient styled

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { BackgroundGradientAnimation } from "./GradientBg";
 
 export const BentoGrid = ({
   className,
@@ -74,18 +75,31 @@ export const BentoGridItem = ({
                     )}
             </div>
 
+            {id === 6 && (
+              <BackgroundGradientAnimation>
+                <div className="absolute z-50 flex 
+                items-center justify-center text-white font-bold"/>
+              </BackgroundGradientAnimation>  
+            )}
+
+            <div className={cn(
+                titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
+            )}>
+                <div className="font-sans font-extralight  z-10
+                text-[#c1c2d3] text-sm md:text-xs lg:text-base
+                ">
+                    {description}
+                </div>
+
+           <div className="font-sans font-bold
+            text-lg lg:text-3xl max-w-96 z-10">
+                 {title}
+            </div>
+
+            </div>
+
         </div>
 
-
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-       
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-          {title}
-        </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
-        </div>
-      </div>
     </div>
   );
 };
--- data/index.ts
+++ data/index.ts
@@ -7,14 +7,14 @@ export const navItems = [
   
   export const gridItems = [
     {
-      id: 1,
-      title: "I prioritize client collaboration, fostering open communication ",
-      description: "",
-      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-      imgClassName: "w-full h-full",
-      titleClassName: "justify-end",
-      img: "/b1.svg",
-      spareImg: "",
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-2 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
     },
     {
       id: 2,
