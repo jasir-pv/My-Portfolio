e45448363c037096e6b7f502a287cf84a6ea63b1
jasir-pv <jasirahsanpv40@gmail.com>
2025-01-28 19:07:14 +0530

    footer work completed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,14 +5,8 @@ import { socialMedia } from '@/data'
 
 const Footer = () => {
   return (
-    <footer className='w-full pt-20 pb-10 ' id='contact'>
-      <div className='w-full absolute left-0 -bottom-72 min-h-96'>
-        <img 
-            src='/footer-grid.svg'
-            alt='grid' 
-            className='w-full h-full opacity-80'
-         />
-      </div>
+    <footer className='w-full pb-10  mb-[100px]' id='contact'>
+
 
       <div className="flex flex-col items-center">
         <h1 className='heading lg:max-w-[45vw]'>
@@ -34,19 +28,20 @@ const Footer = () => {
 
       <div className='flex mt-16 md:flex-row flex-col justify-between items-center'>
         <p className='md:text-base text-sm md:font-normal font-light'> 
-            Copyright © 2024 Jasir Ahsan Pv</p>
-      </div>
+            Copyright © 2024 Jasir Ahsan Pv
+            </p>
 
-
-      <div className='flex items-center md:gap-3'>
+      <div className='flex items-center md:gap-3 gap-4 mt-2'>
         {socialMedia.map((profile) => (
             <div key={profile.id}
             className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"             >
-                <img src={profile.img} alt={profile.id}
+                <img src={profile.img} alt={'icons'}
                     width={20} height={20}
                 />
             </div>
         ))}
+      </div>
+
       </div>
     </footer>
   )
