d4c002d0c9f9b60b1a19855775d5e8b3f544ff83
jasir-pv <jasirahsanpv40@gmail.com>
2025-02-27 13:23:24 +0530

    updated: Resume updated

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -14,7 +14,7 @@ const RecentProjects = () => {
       </h1>
 
       <div className="flex flex-wrap items-center justify-center 
-      p-4 gap-x-24 gap-y-8 mt-2"> 
+      p-6 gap-x-24 gap-y-8 mt-2"> 
           {projects.map(( {id, title, des, img, iconLists, link})=>(
             <div key={id} 
             className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'>
